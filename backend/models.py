3203fb39113ed1a34b8f54362a94c4b61d5daffd
ionut <ionut.epure@gmail.com>
2023-11-18 20:54:32 +0200

    Add backend server

---
+++ backend/models.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+
+class ExpandModel:
+    def __init__(self, context, extra_context = None) -> None:
+        self.client = OpenAI(
+            api_key="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",
+        )
+        self.model = 'gpt-3.5-turbo'
+        self.default_messages = [{"role": "system", "content": "You need to provide more details related to a specific paragraph or phrase depending on the following context: " + context}]
+
+        if extra_context:
+            self.default_messages += [{"role": "system", "content": "Here is some extra context for the required details: " + extra_context}]
+
+        
+
+    def expand(self, phrase) -> None:
+        response = self.client.chat.completions.create(
+            model = self.model,
+            messages = self.default_messages + [{"role": "user", "content": phrase}]
+        )
+
+        return response.choices[0].message.content
+
